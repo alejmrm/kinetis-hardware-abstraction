6438901e95b240dd96865acd20f154cd743282c5
Gray <gray@stokeware.com>
2012-12-21 21:00:12 -0500

    Add nested vectored interrupt controller driver class source and header files. see #7

---
+++ code/common/MCU_NVICDriver.h
@@ -0,0 +1,34 @@
+/*****************************************************************************************************************************************************
+*
+*  MCU_NVICDriver.h  -  Copyright 2012, stokeware
+*
+*  This file contains the MCU nested vectored interrupt controller driver class interface.
+*
+*****************************************************************************************************************************************************/
+#ifndef MCU_NVIC_DRIVER_H
+#define MCU_NVIC_DRIVER_H
+
+#include "MCU_PeriphDriver.h"
+
+/*****************************************************************************************************************************************************
+*
+*  Class Definitions
+*
+*****************************************************************************************************************************************************/
+/*
+ * This class is the NVIC driver class. It is derived from the peripheral driver abstract base class.
+ */
+class NVICDriver : public MCUPeriphDriver
+{
+  /******************
+   * Public Methods
+   *****************/
+  public:
+  /*
+   * These methods are the constructor and destructor for the NVIC driver class.
+   */
+  NVICDriver(void);
+  ~NVICDriver(void);
+};
+
+#endif
