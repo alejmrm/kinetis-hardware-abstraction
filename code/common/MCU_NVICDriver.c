b4493f7e15b6c09a23417eb8bacfd4eb9f112f60
Gray <gray@stokeware.com>
2012-12-22 14:42:13 -0500

    Implement vector table in NVIC driver file. Remove assembly language startup file with vector table definition. see #7

--- code/common/MCU_NVICDriver.c
+++ code/common/MCU_NVICDriver.c
@@ -11,6 +11,180 @@
 
 /*****************************************************************************************************************************************************
 *
+*  Constant Definitions
+*
+*****************************************************************************************************************************************************/
+/*
+ * These constants define the number of interrupt service routines contained in the vector table.
+ */
+#define NUM_VECTOR_TABLE_ISR  110
+
+/*****************************************************************************************************************************************************
+*
+*  Type Definitions
+*
+*****************************************************************************************************************************************************/
+/*
+ * This type defines the signature of interrupt service routines appearing in the vector table.
+ */
+typedef void vector_table_isr(void);
+/*
+ *
+ */
+typedef struct {
+  void*             stackPtr;
+  vector_table_isr* isr[NUM_VECTOR_TABLE_ISR];
+} vector_table;
+
+/*****************************************************************************************************************************************************
+*
+*  Private Methods
+*
+*****************************************************************************************************************************************************/
+/*
+ * This function is the default interrupt service routine for interrupts that have not been assigned a specific handler function.
+ */
+static void DefaultIsr(void)
+{
+  while (true)  { }
+}
+
+/*****************************************************************************************************************************************************
+*
+*  Private Data
+*
+*****************************************************************************************************************************************************/
+/*
+ * This static constant variable ...
+ */
+#define vectorTable __vector_table
+#pragma language=extended
+#pragma segment="CSTACK"
+
+extern "C"  {
+  void __iar_program_start(void);
+}
+
+#pragma location = ".intvec"
+
+static const vector_table vectorTable = {
+  __sfe("CSTACK"),
+  {
+    __iar_program_start,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr,
+    DefaultIsr
+  }
+};
+
+/*****************************************************************************************************************************************************
+*
 *  Public Methods
 *
 *****************************************************************************************************************************************************/
@@ -31,9 +205,7 @@ NVICDriver::~NVICDriver(void)
 /*
  * This static method initializes the NVIC module.
  */
-#pragma section = ".intvec"
-
 void NVICDriver::InitModule(void)
 {
-  SCB_VTOR = (unsigned int)__segment_begin(".intvec");
+  SCB_VTOR = (uint32)&vectorTable;
 }
