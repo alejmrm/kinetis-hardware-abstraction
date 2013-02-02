4f0d0ec9a5b48c2eb2dfc6497e344b2f8e29185e
Gray <gray@stokeware.com>
2013-02-01 21:09:50 -0500

    Create external watchdog monitor driver class source and header files. see #15

---
+++ code/common/EWMDriver.h
@@ -0,0 +1,43 @@
+/*****************************************************************************************************************************************************
+*
+*  EWMDriver.h  -  Copyright 2013, stokeware
+*
+*  This file contains the external watchdog monitor driver class interface.
+*
+*****************************************************************************************************************************************************/
+#ifndef EWM_DRIVER_H
+#define EWM_DRIVER_H
+
+#include "PeriphDriver.h"
+
+/*****************************************************************************************************************************************************
+*
+*  Class Definitions
+*
+*****************************************************************************************************************************************************/
+/*
+ * This class is the EWM driver class. It is derived from the peripheral driver abstract base class.
+ */
+class EWMDriver : public PeriphDriver
+{
+  /******************
+   * Public Methods
+   *****************/
+  public:
+  /*
+   * These methods are the constructor and destructor for the EWM driver class.
+   */
+  EWMDriver(void);
+  ~EWMDriver(void);
+
+  /****************
+   * Private Data
+   ***************/
+  private:
+  /*
+   * This static data member is a pointer to the EWM register structure.
+   */
+  static EWM_MemMapPtr moduleReg;
+};
+
+#endif
