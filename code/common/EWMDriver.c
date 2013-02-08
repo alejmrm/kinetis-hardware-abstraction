df90ec93abf4af323b9de216a9d0d619f750329d
Gray <gray@stokeware.com>
2013-02-07 23:13:11 -0500

    Update Freescale peripheral memory map header file include. see #15

--- code/common/EWMDriver.c
+++ code/common/EWMDriver.c
@@ -5,7 +5,7 @@
 *  This file contains the external watchdog monitor driver class implementation.
 *
 *****************************************************************************************************************************************************/
-#include <freescale/MK40X256VMD100.h>
+#include "MK40DZ10.h"
 #include "CommonTypes.h"
 #include "EWMDriver.h"
 
